2696c8c6633128631d38dd84dafb6498e01e8b15
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-02-21 13:45:03 -0500

    Rename navbar Users link to User Management

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ const onLogout = async () => {
         <RouterLink class="app-brand" to="/">Time Tracking</RouterLink>
         <template v-if="isAuthenticated">
           <RouterLink class="app-nav-link" to="/">Home</RouterLink>
-          <RouterLink v-if="isAdmin" class="app-nav-link" to="/admin/users">Users</RouterLink>
+          <RouterLink v-if="isAdmin" class="app-nav-link" to="/admin/users">User Management</RouterLink>
           <button type="button" class="app-logout-button" @click="onLogout">Logout</button>
         </template>
       </nav>
